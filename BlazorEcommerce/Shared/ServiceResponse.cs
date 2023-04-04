e28676d6615bebbdfb7f7f9c572a4d06cf50f3d8
ruhk5 <ruhk5@LAPTOP-00HDJ5UV>
2023-04-04 15:50:22 -0500

    Product Details

--- BlazorEcommerce/Shared/ServiceResponse.cs
+++ BlazorEcommerce/Shared/ServiceResponse.cs
@@ -9,7 +9,7 @@ namespace BlazorEcommerce.Shared
     public class ServiceResponse<T>
     {
         public T? Data { get; set; }
-        public bool Success { get; set; }
-        public string Message { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
     }
 }
